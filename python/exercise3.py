9e8fa4fbe0608319ea14cb770b8d9a8cc144025a
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-02-27 17:37:42 +0400

    add

---
+++ python/exercise3.py
@@ -0,0 +1,10 @@
+# 1.Create a program that takes a string as input and returns the frequency of each character in the string.
+# 2.Write a function that takes a list of integers and returns the second largest number.
+# 3.Implement a function that takes a list of integers and returns a list containing only the unique elements of the input list.
+# 4.Create a program that simulates a dice roll. The program should prompt the user to enter the number of dice and the number of sides on each die, then simulate the roll and print the result.
+# 5.Implement a program that reads a CSV file and displays its contents in a table format.
+# 6.Write a program that takes a string and determines if it's a palindrome.
+# 7.Create a program that generates a random password of a specified length.
+# 8.Write a function that takes two lists as input and returns a list containing the common elements of both lists.
+# 9.Implement a program that takes a list of numbers and returns the average, median, and mode of the numbers.
+# 10.Write a program that prompts the user to enter a date in the format "YYYY-MM-DD" and calculates the number of days between that date and the current date.
