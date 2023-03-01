c26eb7b1efaa049e4a43f1096bd14b99b0d25f7f
TerentievKirill <kirillterentiev@gmail.com>
2023-03-01 17:47:46 +0300

    hbhbh
    jhjh

---
+++ python/homework_api_weather.py
@@ -0,0 +1,8 @@
+<<<<<<< Updated upstream
+=======
+import requests
+# 40c0e0d57e1c5ee90e6ae57a4e17e2c5
+x = requests.get('https://api.openweathermap.org/data/2.5/weather?id={0}&appid={40c0e0d57e1c5ee90e6ae57a4e17e2c5}')
+print(x.status_code)
+print(x)
+>>>>>>> Stashed changes
